852e168a0e802eb1945ab2e315bdabeb4a4b3d39
OlgaGrinyova <grin.chick98@gmail.com>
2023-01-31 21:19:06 +0300

    Add task_1

---
+++ Lesson_1/Task_1.py
@@ -0,0 +1,15 @@
+my_class = 'A' # Создать переменные
+girls = 15
+boys = 14
+print(my_class, girls, boys) # Вывести переменные на экран
+
+name = input('Введите ваше имя: ')  # Запросить у пользователя несколько чисел и строк
+county = input('Введите страну проживания: ')
+age = int(input('Введите ваш возраст: '))
+birth_year = int(input('Введите вашу дату рождения: '))
+
+name = 'Olga' # Сохранить запрошенные данные в переменные
+country = 'Belarus'
+age = 24
+birth_year = 1998
+print(name, country, age, birth_year) # Вывести значения переменных на экран
