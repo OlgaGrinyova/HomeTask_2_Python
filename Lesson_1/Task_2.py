2390e05d63cc2e34987d522d60cbe46db69497c1
OlgaGrinyova <grin.chick98@gmail.com>
2023-02-01 15:47:20 +0300

    Overwrite task_2

--- Lesson_1/Task_2.py
+++ Lesson_1/Task_2.py
@@ -1,6 +1,14 @@
 time = int(input('Введите время в секундах: '))
+time_limit = 86400
+if time > time_limit:
+    print('Время введено некорректно')
+else:
+    hours = time // 3600 # введенное время делим на 3600 сек и берем целое число
+    minutes = (time - hours * 3600) // 60 # от введенного времени отнимаем часы * 3600 и делим на количество секунд в минуте
+    seconds = time - (hours * 3600 + minutes * 60)
+    print(f'time {hours:02}:{minutes:02}:{seconds:02}')
 
-hours = time // 3600 # введенное время делим на 3600 сек и берем целое число
-minutes = (time - hours * 3600) // 60 # от введенного времени отнимаем часы * 3600 и делим на количество секунд в минуте
-seconds = time - (hours * 3600 + minutes * 60)
-print(f'time: {hours:02}:{minutes:02}:{seconds:02}')
+
+
+
+
